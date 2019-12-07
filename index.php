b6302e4cdedd766501b789697ff96165eecfdf75
cusmirlucian <53294551+cusmirlucian@users.noreply.github.com>
2019-12-07 17:55:55 +0200

    Website
    website

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PinkRhino</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+</head>
+<body>
+	<form class="navbar">
+		<h1>PinkRhino / LOGIN PAGE</h1>
+	</form>
+	<form class="login-form">
+		<form method="POST" action="login.inc.php">
+			<input type="text" name="email" placeholder="Email">
+			<input type="text" name="pass"	placeholder="Password">
+			<input type="submit" value="Login">
+		</form>dwadawd
+	</form>
+</body>
+</html>
